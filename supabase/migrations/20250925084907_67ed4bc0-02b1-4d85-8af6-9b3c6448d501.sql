4df7dfa83dca2f41b5325a4097df9227bd40d107
Abde-Mazgoura <2006013000249@ofppt-edu.ma>
2025-10-01 16:03:42 +0000

    force

--- supabase/migrations/20250925084907_67ed4bc0-02b1-4d85-8af6-9b3c6448d501.sql
+++
@@ -1,4 +0,0 @@
--- Activer RLS sur toutes les tables qui en ont besoin
-ALTER TABLE public.attestation_requests ENABLE ROW LEVEL SECURITY;
-ALTER TABLE public.attestation_counter ENABLE ROW LEVEL SECURITY;
-ALTER TABLE public.students ENABLE ROW LEVEL SECURITY;
